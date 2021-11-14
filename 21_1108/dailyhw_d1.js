3bffd597e46b6f10ead6bf7b9068e02761ca656c
Jinook Jung <jinook929@gmail.com>
2021-11-13 22:28:38 -0500

    cleaned up code

--- 21_1108/dailyhw_d1.js
+++ 21_1108/dailyhw_d1.js
@@ -3,7 +3,7 @@
 //가로(300) x $3.5 + 세로(200) x $3.5
 //300 cm= 9.84252 feet 200cm= 6.56168feet
 //vp = 가로 가격 / hp= 세로 가격 tp = 토탈가격
-const vp = 9.84252 * 3.5
-const hp = 6.56168 * 3.5
-const tp = vp + hp
-console.log(tp+"$")
+const vp = 9.84252 * 3.5;
+const hp = 6.56168 * 3.5;
+const tp = vp + hp;
+console.log("$" + tp.toFixed(2));
